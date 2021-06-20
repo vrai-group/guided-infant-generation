1788be9d42abc194b117b8f757f0039b73075eb4
Giuseppe Pio Cannata <thebloodofjazz29@gmail.com>
2021-06-20 17:04:47 +0200

    Modifiche per adattamento a BabyPose

---
+++ datasets/BabyPose.py
@@ -0,0 +1,124 @@
+"""
+Questo codice rilascia un reader per la lettura del TFRecord del dataset Market
+"""
+import os
+import pdb
+import pickle
+import sys
+import tensorflow as tf
+from utils import utils_wgan
+
+# Per maggiori info su tf.records vedi: https://towardsdatascience.com/a-practical-guide-to-tfrecords-584536bc786c
+_FILE_PATTERN = '%s_%s_*.tfrecord'
+
+class BabyPose():
+    def __init__(self):
+        self.keypoint_num = 14
+        self.example_description = {
+
+          'pz_0': tf.io.FixedLenFeature([], tf.string),  # nome del pz
+          'pz_1': tf.io.FixedLenFeature([], tf.string),
+
+          'image_name_0': tf.io.FixedLenFeature([], tf.string),  # nome img
+          'image_name_1': tf.io.FixedLenFeature([], tf.string),
+          'image_raw_0': tf.io.FixedLenFeature([], tf.string),  # condizioni al contorno
+          'image_raw_1': tf.io.FixedLenFeature([], tf.string),  # GT
+
+          #'image_format': tf.io.FixedLenFeature([], tf.string, default_value='png'),
+          'image_height': tf.io.FixedLenFeature([], tf.int64, default_value=96),
+          'image_width': tf.io.FixedLenFeature([], tf.int64, default_value=128),
+
+          'pose_peaks_0': tf.io.FixedLenFeature([8 * 16 * 14], tf.float32),
+          'pose_peaks_1': tf.io.FixedLenFeature([8 * 16 * 14], tf.float32),
+
+          'pose_mask_r4_0': tf.io.FixedLenFeature([96 * 128 * 1], tf.int64),
+          'pose_mask_r4_1': tf.io.FixedLenFeature([96 * 128 * 1], tf.int64),
+
+          'indices_r4_0': tf.io.FixedLenFeature((),dtype=tf.string),
+          'values_r4_0': tf.io.FixedLenFeature((),dtype=tf.string),
+          'indices_r4_1': tf.io.FixedLenFeature((),dtype=tf.string),
+          'values_r4_1': tf.io.FixedLenFeature((),dtype=tf.string),
+          'shape_len_indices_0': tf.io.FixedLenFeature([], tf.int64),
+          'shape_len_indices_1': tf.io.FixedLenFeature([], tf.int64),
+    }
+
+
+    # ritorna un TF.data
+    def get_unprocess_dataset(self, dataset_dir, name_tfrecord):
+        # deve sempre ritornare uno o piu elementi
+        def _decode_function(example_proto):
+            example = tf.io.parse_single_example(example_proto, self.example_description)
+
+            # IMAGE
+            image_raw_0 = tf.reshape(tf.io.decode_raw(example['image_raw_0'], tf.uint16), [96, 128, 1])
+            image_raw_1 = tf.reshape(tf.io.decode_raw(example['image_raw_1'], tf.uint16), [96, 128, 1])
+
+            # POSE
+            shape_len_indices_0 = example['shape_len_indices_0']
+            pose_0 = tf.sparse.SparseTensor(
+                indices=tf.reshape(tf.io.decode_raw(example['indices_r4_0'], tf.int64), [shape_len_indices_0, 3]),
+                values=tf.io.decode_raw(example['values_r4_0'], tf.int64),
+                dense_shape=[96, 128, self.keypoint_num])
+
+            shape_len_indices_1 = example['shape_len_indices_1']
+            pose_1 = tf.sparse.SparseTensor(
+                indices=tf.reshape(tf.io.decode_raw(example['indices_r4_1'], tf.int64), [shape_len_indices_1, 3]),
+                values=tf.io.decode_raw(example['values_r4_1'], tf.int64),
+                dense_shape=[96, 128, self.keypoint_num])
+
+            # POSE_MASK
+            mask_0 = tf.reshape(example['pose_mask_r4_0'], (96, 128, 1))
+            mask_1 = tf.reshape(example['pose_mask_r4_1'], (96, 128, 1))
+
+            return image_raw_0, image_raw_1, pose_0, pose_1, mask_0, mask_1
+
+        file_pattern = os.path.join(dataset_dir, name_tfrecord)  # poichè la sintassi del file pateern è _FILE_PATTERN = '%s_%s_*.tfrecord'
+        reader = tf.data.TFRecordDataset(file_pattern)
+        dataset = reader.map(_decode_function)
+
+        return dataset
+
+    # ritorna un TF.data preprocessato in G1
+    def get_preprocess_G1_dataset(self, unprocess_dataset):
+        def _preprocess_G1(image_raw_0, image_raw_1, pose_0, pose_1, mask_0, mask_1 ):
+
+            image_raw_0 = utils_wgan.process_image(tf.compat.v1.to_float(image_raw_0), 127.5, 127.5)  # rescale in valori di [-1,1]
+            image_raw_1 = utils_wgan.process_image(tf.compat.v1.to_float(image_raw_1), 127.5, 127.5)  # rescale in valori di [-1,1]
+            # print("l1", image_raw_0.shape)
+            # print("l1", image_raw_1.shape)
+
+            pose_1 = tf.cast(tf.sparse.to_dense(pose_1, default_value=0, validate_indices=False), dtype=tf.float32)
+            mask_1 = tf.cast(tf.reshape(mask_1, (96, 128, 1)), dtype=tf.float32)
+            # print("l1", mask_1.shape)
+
+            pose_1 = pose_1 * 2
+            pose_1 = tf.math.subtract(pose_1, 1, name=None)  # rescale tra [-1, 1]
+            # print("l1", pose_1.shape)
+
+            X = tf.concat([image_raw_0, pose_1], axis=-1)
+            Y = tf.concat([image_raw_1, mask_1], axis=-1)
+
+            # print("l2", Y.shape)
+
+            return X, Y
+
+        return unprocess_dataset.map(_preprocess_G1, num_parallel_calls=tf.data.AUTOTUNE)
+
+    # ritorna un TF.data preprocessato in G1 ma con gli output che possono essere utilizzati durante il training della GAN
+    def get_preprocess_GAN_dataset(self, unprocess_dataset):
+        def _preprocess_G1(image_raw_0, image_raw_1, pose_0, pose_1, mask_0, mask_1 ):
+            image_raw_0 = utils_wgan.process_image(tf.compat.v1.to_float(image_raw_0), 127.5,
+                                                   127.5)  # rescale in valori di [-1,1]
+            image_raw_1 = utils_wgan.process_image(tf.compat.v1.to_float(image_raw_1), 127.5,
+                                                   127.5)  # rescale in valori di [-1,1]
+
+            pose_1 = tf.cast(tf.sparse.to_dense(pose_1, default_value=0, validate_indices=False), dtype=tf.float32)
+            mask_1 = tf.cast(tf.reshape(mask_1, (96, 128, 1)), dtype=tf.float32)
+
+            pose_1 = pose_1 * 2
+            pose_1 = tf.math.subtract(pose_1, 1, name=None)  # rescale tra [-1, 1]
+
+            return image_raw_0, image_raw_1, pose_1, mask_1
+        return unprocess_dataset.map(_preprocess_G1, num_parallel_calls=tf.data.AUTOTUNE)
+
+
