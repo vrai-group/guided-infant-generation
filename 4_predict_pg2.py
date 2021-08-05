fac7ea8cfff95fa222f84ac1ceb3754e836267fc
Giuseppe Cannata <thebloodofjazz29@gmail.com>
2021-08-05 10:37:51 +0200

    Aggiunta predizione sulla cGAN

--- 4_predict_pg2.py
+++ 4_predict_pg2.py
@@ -271,75 +271,118 @@ def predict_G1_view_more_epochs(config):
                             if cnt_img_save == img_save:
                                 break
 
-    def predict_conditional_GAN(self):
 
-        # Preprocess Dataset test
-        dataset_test = self.babypose_obj.get_unprocess_dataset(config.data_tfrecord_path, config.name_tfrecord_valid)
-        dataset_test = self.babypose_obj.get_preprocess_GAN_dataset(dataset_test)
-        dataset_test = dataset_test.batch(1)
-        dataset_test = dataset_test.prefetch(tf.data.AUTOTUNE)  # LASCIO DECIDERE A TENSORFLKOW il numero di memoria corretto per effettuare il prefetch
-        test_it = iter(dataset_test)
+def predict_conditional_GAN (config):
+    babypose_obj = BabyPose(config)
 
-        # Carico il modello preaddestrato G1
-        self.model_G1 = G1.build_model(self.config)
-        self.model_G1.load_weights(os.path.join(self.config.weigths_path, 'Model_G1_001-1.029526-5.404243-0.021277-1.276023-6.921112-0.022076.hdf5'))
+    # Preprocess Dataset
+    dataset = babypose_obj.get_unprocess_dataset(config.data_tfrecord_path, config.name_tfrecord_train)
+    dataset = babypose_obj.get_preprocess_GAN_dataset(dataset)
+    dataset = dataset.batch(1)
+    dataset = iter(dataset)
 
-        # Carico il modello preaddestrato GAN
-        self.model_G2 = G2.build_model(self.config)  # architettura Generatore G2
-        # self.model_G2.load_weights(os.path.join(self.config.weigths_path, 'Model_G2_epoch_015-loss_train_0.646448_real_valid_13_real_train_2790.hdf5'))
-        self.model_G2.load_weights('./weights/Model_G2_epoch_035-loss_train_2.730875_real_valid_79_real_train_4634.hdf5')
-        self.model_D = Discriminator.build_model(self.config)
-        # self.model_D.load_weights(os.path.join(self.config.weigths_path, 'Model_G2_epoch_015-loss_train_2.855738_real_valid_13_real_train_2790.hdf5'))
-        self.model_D.load_weights('./weights/Model_D_epoch_035-loss_train_0.659629_real_valid_79_real_train_4634.hdf5')
+    # Carico il modello preaddestrato G1
+    model_G1 = G1.build_model(config)
+    model_G1.load_weights(os.path.join(config.weigths_path,'Model_G1_epoch_200-loss_0.000185-mse_inf-ssim_0.094355-mask_ssim_0.811881-val_loss_0.000289-val_mse_inf-val_ssim_0.027771_val_mask_ssim_0.799022.hdf5'))
 
-        for cnt in range(int(self.config.dataset_valid_len / 1)):
+    # Carico il modello preaddestrato GAN
+    # G2
+    model_G2 = G2.build_model(config)  # architettura Generatore G2
+    model_G2.load_weights(os.path.join(config.weigths_path, 'a.hdf5'))
+    # D
+    # model_D = Discriminator.build_model(config)
+    # model_D.load_weights(os.path.join(config.weigths_path, 'Model_D_epoch_352-loss_0.689711-loss_values_D_fake_0.345768-loss_values_D_real_0.343946-val_loss_0.687609-val_loss_values_D_fake_0.358670-val_loss_values_D_real_0.328993.hdf5'))
 
-            batch = next(test_it)
-            image_raw_0 = batch[0]  # [batch, 128,64, 3]
-            image_raw_1 = batch[1]  # [batch, 128,64, 3]
-            pose_1 = batch[2]  # [batch, 128,64, 18]
-            mask_1 = batch[3]  # [batch, 128,64, 1]
+    for id_batch in range(int(config.dataset_train_len / 1)):
+
+        batch = next(dataset)
+        if id_batch > 1000:
+            image_raw_0 = batch[0]  # [batch, 96, 128, 1]
+            image_raw_1 = batch[1]  # [batch, 96,128, 1]
+            pose_1 = batch[2]  # [batch, 96,128, 14]
+            mask_1 = batch[3]  # [batch, 96,128, 1]
+            mask_0 = batch[4]  # [batch, 96,128, 1]
+            pz_0 = batch[5]  # [batch, 1]
+            pz_1 = batch[6]  # [batch, 1]
+            name_0 = batch[7]  # [batch, 1]
+            name_1 = batch[8]  # [batch, 1]
 
             # G1
-            input_G1 = tf.concat([image_raw_0, pose_1], axis=-1)  # [batch, 128,64, 21]
-            output_G1 = self.model_G1(input_G1)  # output_g1 --> [batch, 128, 64, 3]
+            input_G1 = tf.concat([image_raw_0, pose_1], axis=-1)  # [batch, 96, 128, 15]
+            output_G1 = model_G1(input_G1)  # output_g1 --> [batch, 96, 128, 1]
+            output_G1 = tf.cast(output_G1, dtype=tf.float16)
 
             # G2
-            input_G2 = tf.concat([output_G1, image_raw_0], axis=-1)  # [batch, 128, 64, 6]
-            output_G2 = self.model_G2(input_G2)  # [batch, 128, 64, 3]
-
-            #Deprocess
-            output_G1 = tf.clip_by_value((output_G1 * 127.5) + 127, 0, 255)
-            output_G1 = tf.cast(output_G1, dtype=tf.int32)[0]
-
-            output_G2 = tf.clip_by_value((output_G2 * 127.5) + 127, 0, 255)
-            output_G2 = tf.cast(output_G2, dtype=tf.int32)[0]
-
-            image_raw_0 = utils_wgan.unprocess_image(image_raw_0, 127.5, 127.5)
-            image_raw_0  = tf.cast(image_raw_0 , dtype=tf.int32)[0]
-
-            image_raw_1 = utils_wgan.unprocess_image(image_raw_1, 127.5, 127.5)
-            image_raw_1 = tf.cast(image_raw_1, dtype=tf.int32)[0]
-
-
-
+            input_G2 = tf.concat([output_G1, image_raw_0], axis=-1)  # [batch, 96, 128, 2]
+            output_G2 = model_G2(input_G2)  # [batch, 96, 128, 1]
+            output_G2 = tf.cast(output_G2, dtype=tf.float16)
             refined_result = output_G1 + output_G2
 
+            # Unprocess
+            image_raw_0 = utils_wgan.unprocess_image(image_raw_0, 1, 32765.5)
+            image_raw_0 = tf.cast(image_raw_0, dtype=tf.float32)[0].numpy()
+
+            # image_raw_1 = utils_wgan.unprocess_image(image_raw_1, 1, 32765.5)
+            # image_raw_1 = tf.cast(image_raw_1, dtype=tf.float32)[0].numpy()
+
+            pose_1 = pose_1.numpy()[0]
+            pose_1 = tf.math.add(pose_1, 1, name=None)  # rescale tra [-1, 1]
+            pose_1 = pose_1 / 2
+            pose_1 = tf.reshape(pose_1, [96, 128, 14]) * 255
+            pose_1 = tf.math.reduce_sum(pose_1, axis=-1).numpy().reshape(96, 128, 1)
+            pose_1 = tf.cast(pose_1, dtype=tf.float32)
+
+            mask_1 = tf.cast(mask_1, dtype=tf.int16)[0].numpy().reshape(96, 128, 1)
+            mask_0 = tf.cast(mask_0, dtype=tf.int16)[0].numpy().reshape(96, 128, 1) * 255
+
+            refined_result = tf.cast(utils_wgan.unprocess_image(refined_result, 350, 32765.5),  dtype=tf.uint16)[0]
+            image_raw_1 = tf.cast(utils_wgan.unprocess_image(image_raw_1, 350, 32765.5),  dtype=tf.uint16)[0]
+
+            mask_image_raw_1 = mask_1 * image_raw_1
+            mask_refined_result = mask_1 * refined_result
+
+            result = tf.image.ssim(mask_image_raw_1, mask_refined_result, max_val=tf.math.reduce_max(image_raw_1))
+
+
+
+            # result = tf.image.ssim(refined_result, image_raw_1, max_val=tf.math.reduce_max(image_raw_1))
+            print(result)
+
+            output_G1 = tf.clip_by_value(utils_wgan.unprocess_image(output_G1, 400, 32765.5), clip_value_min=0,
+                                              clip_value_max=32765)
+            output_G1 = tf.cast(output_G1, dtype=tf.float32)[0]
+
+            output_G2 = tf.clip_by_value(utils_wgan.unprocess_image(output_G2, 400, 32765.5), clip_value_min=0,
+                                         clip_value_max=32765)
+            output_G2 = tf.cast(output_G2, dtype=tf.float32)[0]
+
+            #Save img
+            # import cv2
+            # refined_result = tf.cast((refined_result*32765.5)+350, dtype=tf.uint16)[0]
+            # cv2.imwrite("t.png", refined_result.numpy())
+
+
+            # Predizione D
+            # input_D = tf.concat([image_raw_1, refined_result, image_raw_0],
+            #                     axis=0)  # [batch * 3, 96, 128, 1] --> batch * 3 poichè concateniamo sul primo asse
+            # output_D = self.model_D(input_D)  # [batch * 3, 1]
+            # output_D = tf.reshape(output_D, [-1])  # [batch*3]
+            # output_D = tf.cast(output_D, dtype=tf.float16)
+            # D_pos_image_raw_1, D_neg_refined_result, D_neg_image_raw_0 = tf.split(output_D, 3)  # [batch]
+
             fig = plt.figure(figsize=(10, 10))
-            columns = 5
+            columns = 6
             rows = 1
-            imgs = [output_G1, output_G2, refined_result, image_raw_0, image_raw_1]
+            imgs = [ output_G1, output_G2, mask_refined_result, pose_1, image_raw_0, mask_image_raw_1 ]
             for i in range(1, columns * rows + 1):
                 fig.add_subplot(rows, columns, i)
                 plt.imshow(imgs[i - 1])
-
-            plt.savefig("test.png")
-            a = input("Premi per continuare ")
-
+            plt.show()
 
 
 if __name__ == "__main__":
     Config_file = __import__('1_config_utils')
     config = Config_file.Config()
     #predict_G1_view_more_epochs(config)
-    predict_G1(config)
+    #predict_G1(config)
+    predict_conditional_GAN(config)
