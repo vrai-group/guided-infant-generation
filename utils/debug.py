fbf76fcad1f81fbb1803e9b8a375c4d241acc171
Giuseppe Cannata <thebloodofjazz29@gmail.com>
2021-09-06 09:54:25 +0200

    Pulizia file

--- utils/debug.py
+++ utils/debug.py
@@ -39,9 +39,9 @@ def debug_pairs_dataset_file():
         'image_name_1': tf.io.FixedLenFeature([], tf.string),
     }
 
-    def _parse_image_function(example_proto):
+    def _parse_image_function(image_features_proto):
         # Parse the input tf.train.Example proto using the dictionary above.
-        return tf.io.parse_single_example(example_proto, image_feature_description)
+        return tf.io.parse_single_image_features(image_features_proto, image_feature_description)
 
     parsed_image_dataset = raw_dataset.map(_parse_image_function)
     dict = {}
@@ -63,13 +63,7 @@ def view_tfrecord():
     import IPython.display as display
     import matplotlib.pyplot as plt
 
-    # raw_dataset = tf.data.TFRecordDataset('../data/tfrecord/radius_10/BabyPose_train.tfrecord')
-    raw_dataset = tf.data.TFRecordDataset('../data/Syntetich_complete/tfrecord/negative_no_flip_camp_5/train_augumentation.tfrecord')
-    #raw_dataset = tf.data.TFRecordDataset(' C:/Users/canna/Desktop/New_BabyPose_valid.tfrecord')
-
-    def _parse_image_function(example_proto):
-        # Parse the input tf.train.Example proto using the dictionary above.
-        return tf.io.parse_single_example(example_proto, image_feature_description)
+    raw_dataset = tf.data.TFRecordDataset('../data/Syntetich/tfrecord/negative_no_flip/BabyPose_train.tfrecord')
 
     image_feature_description = {
         'pz_0': tf.io.FixedLenFeature([], tf.string),  # nome del pz
@@ -83,9 +77,6 @@ def view_tfrecord():
         # 'image_format': tf.io.FixedLenFeature([], tf.string, default_value='png'),
         'image_height': tf.io.FixedLenFeature([], tf.int64, default_value=96),
         'image_width': tf.io.FixedLenFeature([], tf.int64, default_value=128),
-
-        # 'pose_peaks_0': tf.io.FixedLenFeature([8 * 16 * 14], tf.float32),
-        # 'pose_peaks_1': tf.io.FixedLenFeature([8 * 16 * 14], tf.float32),
 
         "original_peaks_0": tf.io.FixedLenFeature((), dtype=tf.string),
         "original_peaks_1": tf.io.FixedLenFeature((), dtype=tf.string),
@@ -103,21 +94,24 @@ def view_tfrecord():
         'shape_len_indices_1': tf.io.FixedLenFeature([], tf.int64),
     }
 
+    def _parse_image_function(image_features_proto):
+        # Parse the input tf.train.Example proto using the dictionary above.
+        return tf.io.parse_single_example(image_features_proto, image_feature_description)
+
     parsed_image_dataset = raw_dataset.map(_parse_image_function)
     cnt=0
     for image_features in parsed_image_dataset:
         cnt += 1
         print(cnt)
 
-        if cnt >= 0:
+        if cnt >= 27:
 
             pz_0 = image_features['pz_0'].numpy().decode('utf-8')
             pz_1 = image_features['pz_1'].numpy().decode('utf-8')
 
             image_name_0 = image_features['image_name_0'].numpy().decode('utf-8')
             image_name_1 = image_features['image_name_1'].numpy().decode('utf-8')
-            print(pz_0, "  ", pz_1)
-            print(image_name_0, "  ", image_name_1)
+            print(pz_0, ": ", image_name_0, "  ",pz_1, " :", image_name_1)
 
             image_raw_0 = tf.reshape(tf.io.decode_raw(image_features['image_raw_0'], tf.uint16), [96, 128, 1]).numpy()
             image_raw_1 = tf.reshape(tf.io.decode_raw(image_features['image_raw_1'], tf.uint16), [96, 128, 1]).numpy()
@@ -132,20 +126,19 @@ def view_tfrecord():
 
             #Peaks
             shape_len_indices_0 = image_features['shape_len_indices_0']
-            indices_r4_0 = tf.io.decode_raw(image_features['indices_r4_0'], np.int64)
-            indices_r4_0 = tf.reshape(indices_r4_0, [shape_len_indices_0, 3])
-            values_r4_0 = tf.io.decode_raw(image_features['values_r4_0'], np.int64)
-            pose_0 = tf.sparse.SparseTensor(indices=indices_r4_0, values=values_r4_0, dense_shape=[96, 128, 14])
+            indices_0 = tf.reshape(tf.io.decode_raw(image_features['indices_r4_0'], tf.int64), [shape_len_indices_0, 3])
+            values_0 = tf.io.decode_raw(image_features['values_r4_0'], tf.int64)
+            pose_0 = tf.sparse.SparseTensor(indices=indices_0, values=values_0, dense_shape=[96, 128, 14])
             pose_0 = tf.sparse.to_dense(pose_0, default_value=0, validate_indices=False)
-            pose_0 = tf.math.reduce_sum(pose_0, axis=-1).numpy().reshape(96,128,1)
+            pose_0 = tf.math.reduce_sum(pose_0, axis=-1).numpy().reshape(96, 128, 1)
 
             shape_len_indices_1 = image_features['shape_len_indices_1']
-            indices_r4_1 = tf.io.decode_raw(image_features['indices_r4_1'], np.int64)
-            indices_r4_1 = tf.reshape(indices_r4_1, [shape_len_indices_1, 3])
-            values_r4_1 = tf.io.decode_raw(image_features['values_r4_1'], np.int64)
-            pose_1 = tf.sparse.SparseTensor(indices=indices_r4_1, values=values_r4_1, dense_shape=[96, 128, 14])
+            indices_1 = tf.reshape(tf.io.decode_raw(image_features['indices_r4_1'], tf.int64), [shape_len_indices_1, 3])
+            values_1 = tf.io.decode_raw(image_features['values_r4_1'], tf.int64)
+            pose_1 = tf.sparse.SparseTensor(indices=indices_1, values=values_1, dense_shape=[96, 128, 14])
             pose_1 = tf.sparse.to_dense(pose_1, default_value=0, validate_indices=False)
-            pose_1 = tf.math.reduce_sum(pose_1, axis=-1).numpy().reshape(96,128,1)
+            pose_1 = tf.math.reduce_sum(pose_1, axis=-1).numpy().reshape(96, 128, 1)
+
 
             #mask
             pose_mask_r4_0 = image_features['pose_mask_r4_0'].numpy().reshape(96, 128, 1)
@@ -154,7 +147,6 @@ def view_tfrecord():
             fig = plt.figure(figsize=(10, 2))
             columns = 6
             rows = 1
-            #imgs = [image_raw_0, image_raw_1, pose_0, pose_1, pose_mask_r4_0 * 255, pose_mask_r4_1 * 255]
             imgs = [image_raw_0 + pose_0*255, image_raw_1 + pose_1*255, image_raw_0 + pose_mask_r4_0*255, image_raw_1 + pose_mask_r4_1*255,  pose_mask_r4_0 * 255, pose_mask_r4_1 * 255]
             for i in range(1, columns * rows + 1):
                 fig.add_subplot(rows, columns, i)
