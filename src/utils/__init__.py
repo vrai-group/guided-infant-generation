1ea8415422e3e6fdfa692795cee612b8c2094751
GiuseppeCannata <thebloodofjazz29@gmail.com>
2022-01-19 17:32:35 +0100

    Aggiunta file int per package

---
+++ src/utils/__init__.py
@@ -0,0 +1,5 @@
+from .augumentation.augumentation import apply_augumentation
+from .evaluation.evaluation_G1 import start
+from .evaluation.evaluation_GAN import start
+
+from utils_methods import int64_feature, float_feature, bytes_feature, save_grid, import_module
