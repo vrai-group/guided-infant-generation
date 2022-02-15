5778d3bea851a9d79e229cf97c4945bfb038d1fd
GiuseppeCannata <thebloodofjazz29@gmail.com>
2022-02-15 10:00:52 +0100

    Modifica .gitignore

--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt install unzip
-wget https://github.com/GiuseppeCannata/BabyPoseGuided/archive/master.zip
-unzip master.zip
-mv BabyPoseGuided-master BabyPoseGuided
-realpath BabyPoseGuided
